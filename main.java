d77842bdc0dd43863284ebfbecc6fdcf8b2ceaed
DrFreddy <fre_rz@hotmail.com>
2015-11-07 10:19:40 -0600

    v2. main

--- main.java
+++ main.java
@@ -6,6 +6,7 @@ public static void main(String[] args) throws Exception
 	{
 		String linea="", textoC="";
 		String[] info,datos;
+		Represent_Graph_Adjacency_Matrix graph;
 //*********************LECTURA DE CIUDADES********************
 		try{
       	File archivo = new File ("src\\ciudades.txt");
@@ -17,39 +18,66 @@ public static void main(String[] args) throws Exception
           while((linea = bf.readLine()) != null){ 
         	  textoC=textoC+linea;
         	  textoC= textoC+"< ";
+        	  cantLineas++;
           }//Cierre del while
           info=textoC.split("< ");
           
      
-          
-         Vector<String> ciudades= new Vector <String>();
+         Grafo<Integer> grafo= new Grafo<Integer>();
+         
           int contador=0;
+          int nodos=0;
+          graph = new Represent_Graph_Adjacency_Matrix(grafo.getCiudades().capacity());
           for (int i=0; i<info.length ; i++){
         	  datos= info[i].split(" ");
         	  /*System.out.println(datos[0]);
         	  System.out.println(datos[1]);
         	  System.out.println(datos[2]);*/
         	 
-        	  if (ciudades.contains(datos[0]) == false){
-        		  ciudades.add(datos[0]);
+        	  if (grafo.getCiudades().contains(datos[0]) == false){
+        		grafo.addNodo(datos[0]);
+        		//grafo.getCiudades().indexOf("");
         	  }
-        	  if(ciudades.contains(datos[1]) == false){
-        		  ciudades.add(datos[1]);
+        	  if(grafo.getCiudades().contains(datos[1]) == false){
+        		  grafo.addNodo(datos[1]);
+        		  grafo.addVertice(grafo.getCiudades().indexOf(datos[0]), grafo.getCiudades().indexOf(datos[1]), Integer.parseInt(datos[2]));
+        		  graph.makeEdge(grafo.getCiudades().indexOf(datos[0]),grafo.getCiudades().indexOf(datos[1]),Integer.parseInt(datos[2]));
         	  }
-        	  
-        	 
+        	  if((grafo.getCiudades().indexOf(datos[0])!= -1) && (grafo.getCiudades().indexOf(datos[0])!= -1)){
+        		  grafo.addVertice(grafo.getCiudades().indexOf(datos[0]), grafo.getCiudades().indexOf(datos[1]), Integer.parseInt(datos[2]));
+        		  graph.makeEdge(grafo.getCiudades().indexOf(datos[0]),grafo.getCiudades().indexOf(datos[1]),Integer.parseInt(datos[2]));
+        	  }
           }
           
-          for (int j=0; j<ciudades.capacity(); j++){
-        	  System.out.println(ciudades.get(j));
-          }
-       	  
+         
           
-     		/* for (int k=0; k<ciudades.length; k++){
-     			 System.out.println(ciudades[k]);
-     			 if (ciudades[k]== null){
-     				 break;
-     			 }*/
+        
+          /*  
+          for (int k=0; k< info.length; k++){
+        		datos= info[k].split(" ");
+        		graph.makeEdge(grafo.getCiudades().indexOf(datos[0]),grafo.getCiudades().indexOf(datos[1]),Integer.parseInt(datos[2]));
+        	}*/
+             
+          
+          int v=grafo.getCiudades().capacity();
+          System.out.println("The adjacency matrix for the given graph is: ");
+          System.out.print("  ");
+          for (int i = 0; i <= v; i++)
+          System.out.print(i + " ");
+          System.out.println();
+
+          for (int i = 0; i <= v; i++) 
+          {
+              System.out.print(i + " ");
+              for (int j = 1; j <= v; j++) 
+                  System.out.print(graph.getEdge(i, j) + " ");
+              System.out.println();
+          }
+          
+          
+          
+          
+          
 		}
 		
 		//Cierre del try
